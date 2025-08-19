116d47010520e1136a8f1ebed7098203830d1748
Andrei Canta <deiucanta@gmail.com>
2025-08-19 18:16:27 +0300

    Fix React externalization - remove packages: bundle option

--- tsup.config.ts
+++ tsup.config.ts
@@ -11,17 +11,17 @@ export default defineConfig({
     'react-dom',
     'react/jsx-runtime',
     'react/jsx-dev-runtime',
-    'react-dom/client'
+    'react-dom/client',
+    'react-dom/server'
   ],
   bundle: true,
   splitting: false,
   treeshake: false,
-  noExternal: [/.*/], // Bundle everything except external
   esbuildOptions(options) {
     options.jsx = 'automatic'
     options.platform = 'browser'
     options.target = 'es2020'
-    options.packages = 'bundle'
+    // Remove packages: 'bundle' - this might be causing the issue
   },
   onSuccess: 'npm run build:css',
 })
